797bbd6612366442ee1002cb8c6e204fb5ce702c
Abhishek <abhisheksoni9421@gmail.com>
2021-12-23 01:08:16 +0530

    string builder

---
+++ src/stringBuilder/StringBuilder.java
@@ -0,0 +1,78 @@
+package stringBuilder;
+public class StringBuilder {
+//toogle cases
+	//abAc-> ABaC
+	public static String toggleCase(String s) {
+		StringBuilder sb = new StringBuilder();
+		for(int i=0;i<s.length();i++) {
+			char c = s.charAt(i);
+			char d;
+			if(c >= 'A' && c<='z') {
+				//if c is Uppercase
+				d=(char)(c - 'A' + 'a');
+			}
+			else {
+				//c is lowercase
+				d = (char)(c-'a'+'A');
+			}
+		sb.append(d);
+		}
+	return sb.toString();
+	}
+	//ASCII difference
+	// acd=> a2c1d
+	// aefbd => a4e1f-4b2d
+	//acd => a2c
+	
+	public static String ASCIIDifference(String s) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(s.charAt(0));
+		
+		for(int i = 1; i< s.length(); i++) {
+			char curr = s.charAt(i);
+			char prev = s.charAt(i-1);
+			int diff = curr - prev;
+		  sb.append(diff);
+		  sb.append(curr);
+		}
+		return sb.toString();
+		
+		}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  StringBuilder sb = new StringBuilder("Hey");
+  System.out.println(sb);
+	//get char
+   char c = sb.charAt(0);
+   System.out.println(c);
+   
+   // set char
+   char c = sb.charAt(1,'a');
+   System.out.println(sb);
+   
+   //insert or add at index
+   sb.insert(1,'e');
+   
+   //add in end
+   sb.append('c');
+   System.out.println(sb);
+   
+   //remove 
+   sb.deletecharAt(1);
+  System.out.println(sb);
+  
+  sb.append('d');
+  System.out.println(sb.length());
+  System.out.println(sb);
+  
+  String s = sb.toString();
+  System.out.println(s);
+  //System.out.println("-----------------");
+  //System.out.println(toggleCase("ABcdeFS"));
+  //System.out.println(ASCIIDiference("aefbd"));
+  
+   
+	}
+
+}
---
+++ src/stringBuilder/Strings.java
@@ -0,0 +1,87 @@
+package stringBuilder;
+
+public class Strings {
+  public static void printAllSubstrings(String s) {
+	  for(int start = 0; start < s.length(); start++) {
+		  for(int end = start + 1; end <= s.length(); end++) {
+			  System.out.println(s.substring(start,end));
+		  }
+	  }
+  }
+	public static boolean isPalindrome(String s) {
+		int left = 0;
+		int right = s.length()-1;
+		while(left<right) {
+			char lchar = s.charAt(left);
+			char rchar = s.charAt(right);
+			if(lchar != rchar) {
+				return false;
+			}
+		left++;
+		right--;
+		}
+	return true;
+	
+	}
+// print all palindromic substrings
+	public static void printAllPalindromicSubstrings(String s) {
+		for(int start = 0; start<s.length(); start++) {
+			for(int end = start+1; end<=s.length(); end++) {
+//				System.out.println(s.substring(start,end));
+				if(isPalindrome(s.substring(start,end))) {
+					System.out.println(s.substring(start, end));
+				}
+			}
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     String s =" naman";
+  //   s+=10;
+//   System.out.println(s)	;
+   // System.out.println(10+20+"Hi"+10+20);
+     
+     //char c = s.charAt(4);
+    // System.out.println(c);
+    
+     // System.out.println(s.substring(1,5));
+     // System.out.println(s.substring(6));
+     
+     // String strArr[] = s.split("H");
+     // for(int i=0; i<strArr.length; i++)
+    // {
+    //	 System.out.println(strArr[i]);
+    //	 }
+    //length() 
+     // for(int i=0;i<s.length();i++){
+   //  char a = s.charAt(i);
+     // System.out.println(a);
+//	}
+//    s.toCharArray()
+     // printAllSubstrings("Hey");
+     // System.out.println(isPalindrome("123214"));
+     // char c = 'e';
+     // System.out.println(c);
+  //   System.out.println(c-2);
+     // System.out.println((char)(c-2));
+     // for(int i= 0;i<26;i++){
+     // System.out.println(char)('a'+i)+"->" + ('a'+i));
+//	}
+//    printAllpalindromicSubstrings(s);
+     String a = "Hey How Are You";
+     String arr[] = a.split(" ");
+     for(int i=0; i<arr.length; i++) {
+    	 System.out.println(i+"index,:"+arr[i]);
+    	 
+     }
+	char arr2[] = a.toCharArray();
+	for(int i=0; i<arr2.length; i++) {
+		System.out.println(arr2[i]);
+		
+	}
+System.out.println((int)' ');
+  String q = "adadadf";
+  isPalindrome(q);
+	}
+
+}
